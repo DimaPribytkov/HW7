7f099156d9c6cf8d489de10dafdfb02b9c9fe842
ДимаПрибытков <d.pribytkov93@mail.ru>
2023-12-04 23:13:29 +0300

    Сделал рефакторинг

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       // int toadEyesCount = 0;          //Жабьи глаза
-       // int ghoulTearsCount = 0;        //Слезы вурдалака
-       // int ravenBonesCount = 0;        //Кости ворона
-       // int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
+        int toadEyesCount = 0;          //Жабьи глаза
+        int ghoulTearsCount = 0;        //Слезы вурдалака
+        int ravenBonesCount = 0;        //Кости ворона
+        int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
 
-        //todo реализовать ввод пользователем кол-ва ингредиентов.
+        System.out.println("\t\t\t ***Добро пожаловать в лабораторию!***\n " +
+                "\t\t ***Введите нужное количество ингридиентов!***");
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
@@ -15,67 +16,74 @@ public class Main {
         // 3. 7 слез вурдалака + 1 пельмень + 2 жабьих глаза - Эликсир скрытности
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
-        //todo дописать логику программы сюда
-        boolean isContinue = true;
-        while (isContinue) {
+        boolean isCanCook = false;
+        while (!isCanCook) {
             System.out.println("Введите количество жабьих глаз");
-            int toadEyesCount = new Scanner(System.in).nextInt();
+            toadEyesCount = new Scanner(System.in).nextInt();
 
             System.out.println("Введите количество слез вурдалака");
-            int ghoulTearsCount = new Scanner(System.in).nextInt();
+            ghoulTearsCount = new Scanner(System.in).nextInt();
 
             System.out.println("Введите количество костей вороны");
-            int ravenBonesCount = new Scanner(System.in).nextInt();
+            ravenBonesCount = new Scanner(System.in).nextInt();
 
-            System.out.println("Введите количество 3пельменей");
-            int dumplingsCount = new Scanner(System.in).nextInt();
+            System.out.println("Введите количество пельменей");
+            dumplingsCount = new Scanner(System.in).nextInt();
 
-            if (toadEyesCount == 3 && ravenBonesCount == 1) {
-                System.out.println("Вы можете приготовит Эликсир зоркости");
+            if (toadEyesCount >= 3 && ravenBonesCount >= 1) {
+                System.out.println("Вы можете приготовить Эликсир зоркости");
+                isCanCook = true;
             }
-            if (ravenBonesCount == 2 && dumplingsCount == 4) {
-                System.out.println("Вы можете приготовит Эликсир стойкости");
+            if (ravenBonesCount >= 2 && dumplingsCount >= 4) {
+                System.out.println("Вы можете приготовить Эликсир стойкости");
+                isCanCook = true;
             }
-            if (ghoulTearsCount == 7 && dumplingsCount == 1 && toadEyesCount == 2) {
-                System.out.println("Вы можете приготовит Эликсир скрытности");
+            if (ghoulTearsCount >= 7 && dumplingsCount >= 1 && toadEyesCount >= 2) {
+                System.out.println("Вы можете приготовить Эликсир скрытности");
+                isCanCook = true;
             }
-            if (ghoulTearsCount == 5 && dumplingsCount == 10 && toadEyesCount == 4 && ravenBonesCount == 3) {
-                System.out.println("Вы можете приготовит Запретный элексир");
-            } else {
-                System.out.println("У вас недостаточное количество ингридиентов");
+            if (ghoulTearsCount >= 5 && dumplingsCount >= 10 && toadEyesCount >= 4 && ravenBonesCount >= 3) {
+                System.out.println("Вы можете приготовить Запретный эликсир");
+                isCanCook = true;
+            } else if (!isCanCook) {
+                System.out.println("Вы не можете приготовить ни один эликсир");
             }
 
 
 
-
-       /* System.out.println("Введите количество жабьих глаз");
-        int toadEyesCount = new Scanner(System.in).nextInt();
+        /*boolean isCanCook = false;
+        System.out.println("Введите количество жабьих глаз");
+        toadEyesCount = new Scanner(System.in).nextInt();
 
         System.out.println("Введите количество слез вурдалака");
-        int ghoulTearsCount = new Scanner(System.in).nextInt();
+        ghoulTearsCount = new Scanner(System.in).nextInt();
 
         System.out.println("Введите количество костей вороны");
-        int ravenBonesCount = new Scanner(System.in).nextInt();
+        ravenBonesCount = new Scanner(System.in).nextInt();
 
         System.out.println("Введите количество костей пельменей");
-        int dumplingsCount = new Scanner(System.in).nextInt();
+        dumplingsCount = new Scanner(System.in).nextInt();
 
-        if (toadEyesCount == 3 && ravenBonesCount == 1){
+        if (toadEyesCount >= 3 && ravenBonesCount >= 1){
             System.out.println("Вы можете приготовит Эликсир зоркости");
+            isCanCook = true;
         }
-        if(ravenBonesCount == 2 && dumplingsCount == 4){
+        if(ravenBonesCount >= 2 && dumplingsCount >= 4){
             System.out.println("Вы можете приготовит Эликсир стойкости");
+            isCanCook = true;
         }
-        if(ghoulTearsCount == 7 && dumplingsCount == 1 && toadEyesCount == 2){
+        if(ghoulTearsCount >= 7 && dumplingsCount >= 1 && toadEyesCount >= 2){
             System.out.println("Вы можете приготовит Эликсир скрытности");
+            isCanCook = true;
         }
-        if(ghoulTearsCount == 5 && dumplingsCount == 10 && toadEyesCount == 4 && ravenBonesCount == 3){
+        if(ghoulTearsCount >= 5 && dumplingsCount >= 10 && toadEyesCount >= 4 && ravenBonesCount >= 3){
             System.out.println("Вы можете приготовит Запретный элексир");
+            isCanCook = true;
         }
-        else {
-            System.out.println("У вас не хватает ингридиентов, чтобы приготовить элексир");
-        }
-*/
+        if (isCanCook == false){
+            System.out.println("Вы не можете приготовить ни один эликсир");
+        }*/
+
         /*System.out.println(toadEyesCount);
         System.out.println(ghoulTearsCount);
         System.out.println(ravenBonesCount);
